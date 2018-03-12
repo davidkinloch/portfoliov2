87cfc70322467b456f519b1e4831c3f763aea234
David Kinloch <dkinloch@gmail.com>
2018-03-12 04:28:29 +0100

    realtvie links

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,15 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="apple-touch-icon" sizes="180x180" href="fav/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="fav/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="fav/favicon-16x16.png">
-<link rel="manifest" href="fav/site.webmanifest">
-<link rel="mask-icon" href="fav/safari-pinned-tab.svg" color="#555555">
+<link rel="apple-touch-icon" sizes="180x180" href="/fav/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/fav/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/fav/favicon-16x16.png">
+<link rel="manifest" href="/fav/site.webmanifest">
+<link rel="mask-icon" href="/fav/safari-pinned-tab.svg" color="#555555">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
 
 <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="css/main.css" />
+<link rel="stylesheet" type="text/css" href="/css/main.css" />
 
 <meta name="verify-v1" content="poxjCWv2Q9zou6XMGpSa4bC00fL+5h84OPpfUwzn05k=" />
 <link rel="publisher" href="+DavidKinloch"/>
