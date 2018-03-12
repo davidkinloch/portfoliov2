8e1d02844019e00808cb687d4250b65ce6820b3a
David Kinloch <dkinloch@gmail.com>
2018-03-12 04:32:28 +0100

    https rewrtie

--- includes/end.php
+++ includes/end.php
@@ -1,6 +1,6 @@
 		<!--<script src="ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>-->
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.15.1/TweenMax.min.js"></script>
-        <script src="js/build/production.min.js"></script>
+        <script src="/js/build/production.min.js"></script>
     </body>
 </html>
