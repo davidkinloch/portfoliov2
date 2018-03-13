0c84e3909eeb3ec5c47ca5fde1909085203913ba
David Kinloch <dkinloch@gmail.com>
2018-03-13 20:53:10 +0100

    moved fonts to footer

--- includes/end.php
+++ includes/end.php
@@ -1,4 +1,6 @@
-		<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+		<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="/css/main.css" />
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
         <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.15.1/TweenMax.min.js"></script>
         <script src="/js/build/production.min.js"></script>
--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
 <link rel="apple-touch-icon" sizes="180x180" href="/fav/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/fav/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/fav/favicon-16x16.png">
@@ -7,10 +6,7 @@
 <link rel="mask-icon" href="/fav/safari-pinned-tab.svg" color="#555555">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
-
-<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="/css/main.css" />
-
 <meta name="verify-v1" content="poxjCWv2Q9zou6XMGpSa4bC00fL+5h84OPpfUwzn05k=" />
 <link rel="publisher" href="+DavidKinloch"/>
 <meta property="og:locale" content="en_UK" />
@@ -26,7 +22,6 @@
 <meta name="twitter:site" content="@DavidKinloch" />
 <meta name="twitter:image" content="https://davidkinloch.com/fav/android-chrome-384x384.png" />
 <meta name="twitter:creator" content="@DavidKinloch" />
-
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-6880596-3"></script>
 <script>
