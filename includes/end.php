0c2cfcdbeae05019da600555728b61870b044a26
David Kinloch <dkinloch@gmail.com>
2018-03-13 20:30:18 +0100

    cdn jquery

--- includes/end.php
+++ includes/end.php
@@ -1,4 +1,4 @@
-		<!--<script src="ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>-->
+		<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
         <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.15.1/TweenMax.min.js"></script>
         <script src="/js/build/production.min.js"></script>
