ce423a7fec0fe1923d716675bb59d6b0fff33d2f
David Kinloch <dkinloch@gmail.com>
2018-03-14 07:08:19 +0100

    updated jquery

--- includes/end.php
+++ includes/end.php
@@ -1,7 +1,6 @@
 		<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="/css/main.css" />
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-2.2.4.min.js"><\/script>')</script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.15.1/TweenMax.min.js"></script>
         <script src="/js/build/production.min.js"></script>
     </body>
