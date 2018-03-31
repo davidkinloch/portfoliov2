e03ba573805fa8d701b5cc155e854d1a40cc2956
David Kinloch <dkinloch@gmail.com>
2018-03-31 17:50:12 +0200

    fixed jquery version

--- includes/end.php
+++ includes/end.php
@@ -1,6 +1,6 @@
 		<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-2.2.4.min.js"><\/script>')</script>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.15.1/TweenMax.min.js"></script>
         <script src="/js/build/production.min.js"></script>
     </body>
