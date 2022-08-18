e6cbea8f95077b62487f1a73f1a9eaa21a928d47
David Kinloch <dkinloch@gmail.com>
2022-08-18 12:27:59 +0200

    CV updated

--- includes/contact.php
+++ includes/contact.php
@@ -3,7 +3,7 @@
         <h2 class="section__title">Discuss <strong>project</strong> or <strong>contract</strong> work?</h2>
         <p><a class="button mail" href="mailto:info@davidkinloch.com"><i class="fa fa-envelope"></i> EMAIL ME</a></p>
         <p><a class="button phone" href="tel:+447798780881"><i class="fa fa-phone"></i> CALL ME</a></p>
-        <p><a class="button cv" target="_blank" rel="noopener" href="/CV_Sep_2017.pdf"><i class="fa fa-file-text-o"></i> CV</a></p>
+        <p><a class="button cv" target="_blank" rel="noopener" href="/David_Kinloch_UX_Designer_2022.pdf"><i class="fa fa-file-text-o"></i> CV</a></p>
         <p><a class="button cv" target="_blank" rel="noopener" href="https://github.com/davidkinloch"><i class="fa fa-github"></i> Github</a></p>
         <p><a class="button linkedin" target="_blank" rel="noopener" href="https://www.linkedin.com/in/david-kinloch-57660610/"><i class="fa fa-linkedin-square"></i> Linkedin</a></p>
     </div>
